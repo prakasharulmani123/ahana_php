c54ae25a920e00bc5f6b88934ad86b68e2de5a20
vasanth <vasanth@arkinfotec.com>
2016-05-23 18:31:13 +0530

    Products addtional fields

--- common/models/PhaProduct.php
+++ common/models/PhaProduct.php
@@ -49,6 +49,7 @@ use yii\db\ActiveQuery;
 class PhaProduct extends RActiveRecord {
     
     public $full_name;
+    public $supplier_ids = false;
 
     /**
      * @inheritdoc
@@ -60,19 +61,41 @@ class PhaProduct extends RActiveRecord {
     /**
      * @inheritdoc
      */
+   
     public function rules() {
         return [
             [['product_name', 'product_unit', 'product_unit_count', 'product_description_id', 'product_reorder_min', 'product_reorder_max', 'brand_id', 'division_id', 'generic_id', 'purchase_vat_id', 'purchase_package_id', 'sales_vat_id', 'sales_package_id'], 'required'],
             [['tenant_id', 'product_description_id', 'product_reorder_min', 'product_reorder_max', 'brand_id', 'division_id', 'generic_id', 'drug_class_id', 'purchase_vat_id', 'purchase_package_id', 'sales_vat_id', 'sales_package_id', 'created_by', 'modified_by'], 'integer'],
             [['product_price'], 'number'],
-            [['status'], 'string'],
-            [['created_at', 'modified_at', 'deleted_at'], 'safe'],
+            [['status'], 'string'],            
+            [['product_reorder_max'],'validateReorderchck'],
+            //[['supplier_ids'], 'validateSupplieronetime'],
+            //[['supplier_id_1','supplier_id_2','supplier_id_3'], 'validateSupplieronetime'],
+            [['created_at', 'modified_at', 'deleted_at','supplier_id_1','supplier_id_2','supplier_id_3','supplier_ids'], 'safe'],
             [['product_code'], 'string', 'max' => 50],
             [['product_name', 'product_location'], 'string', 'max' => 255],
             [['product_unit', 'product_unit_count'], 'string', 'max' => 25],
             [['tenant_id', 'product_name'], 'unique', 'targetAttribute' => ['tenant_id', 'product_name'], 'message' => 'The combination of Tenant ID and Product Name has already been taken.']
         ];
     }
+    
+    public function validateReorderchck($attribute, $params) 
+    {
+        if($this->product_reorder_max < $this->product_reorder_min )
+        {    
+            $this->addError($attribute, "Re-Order Level (Min) value is lower than Re-Order Level (Max) value.");    
+        }    
+    }        
+    
+//    public function validateSupplieronetime($attribute, $params) 
+//    {     
+//        if(!$this->supplier_ids && ($this->supplier_id_1==$this->supplier_id_2 || $this->supplier_id_2==$this->supplier_id_3 || $this->supplier_id_3==$this->supplier_id_1))
+//        {
+//            $this->addError($attribute, "Please choose different suppliers.");
+//            $this->supplier_ids = true;
+//        }
+//           
+//    }        
 
     /**
      * @inheritdoc
@@ -104,6 +127,9 @@ class PhaProduct extends RActiveRecord {
             'modified_by' => 'Modified By',
             'modified_at' => 'Modified At',
             'deleted_at' => 'Deleted At',
+            'supplier_id_1' => 'Supplier 1',
+            'supplier_id_2' => 'Supplier 2',
+            'supplier_id_3' => 'Supplier 3',
         ];
     }
 
