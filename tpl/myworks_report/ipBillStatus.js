c9247355b57d975071955a196af849334a907135
s-nadesh <nadesh@arkinfotec.com>
2017-01-06 18:15:30 +0530

    My works- Reports- Print

--- tpl/myworks_report/ipBillStatus.js
+++ tpl/myworks_report/ipBillStatus.js
@@ -1,5 +1,5 @@
 app.controller('ipBillStatusController', ['$rootScope', '$scope', '$timeout', '$http', '$state', '$anchorScroll', '$filter', function ($rootScope, $scope, $timeout, $http, $state, $anchorScroll, $filter) {
-        
+
         $scope.initReport = function () {
             $scope.showTable = false;
             $scope.tenants = [];
@@ -15,14 +15,14 @@ app.controller('ipBillStatusController', ['$rootScope', '$scope', '$timeout', '$
                 }
             });
         }
-        
+
         $scope.clearReport = function () {
             $scope.showTable = false;
             $scope.data = {};
             $scope.data.consultant_id = '';
             $scope.data.tenant_id = '';
         }
-        
+
         //Index Page
         $scope.loadReport = function () {
             $scope.records = [];
@@ -31,14 +31,14 @@ app.controller('ipBillStatusController', ['$rootScope', '$scope', '$timeout', '$
 
             $scope.errorData = "";
             $scope.msg.successMessage = "";
-            
+
             var data = {};
             if (typeof $scope.data.consultant_id !== 'undefined' && $scope.data.consultant_id != '')
                 angular.extend(data, {consultant_id: $scope.data.consultant_id});
 
             if (typeof $scope.data.tenant_id !== 'undefined' && $scope.data.tenant_id != '')
                 angular.extend(data, {tenant_id: $scope.data.tenant_id});
-            
+
             // Get data's from service
             $http.post($rootScope.IRISOrgServiceUrl + '/myworkreports/ipbillstatus', data)
                     .success(function (response) {
@@ -50,4 +50,132 @@ app.controller('ipBillStatusController', ['$rootScope', '$scope', '$timeout', '$
                         $scope.errorData = "An Error has occured";
                     });
         };
+
+        $scope.printHeader = function () {
+            return {
+                text: "IP Bill Status",
+                margin: 5,
+                alignment: 'center'
+            };
+        }
+
+        $scope.printFooter = function () {
+            return {
+                text: [
+                    {
+                        text: 'Report Genarate On : ',
+                        bold: true
+                    },
+                    moment().format('YYYY-MM-DD HH:mm:ss')
+                ],
+                margin: 5
+            };
+        }
+
+        $scope.printStyle = function () {
+            return {
+                header: {
+                    bold: true,
+                    color: '#000',
+                    fontSize: 11
+                },
+                demoTable: {
+                    color: '#000',
+                    fontSize: 10
+                }
+            };
+        }
+
+        $scope.printContent = function () {
+            var generated_on = $scope.generated_on;
+            var generated_by = $scope.app.username;
+
+            var content = [];
+            var branch_wise = $filter('groupBy')($scope.records, 'branch_name');
+
+            var result_count = Object.keys(branch_wise).length;
+            var index = 1;
+            angular.forEach(branch_wise, function (details, branch_name) {
+                var content_info = [];
+
+                var branches = [];
+                branches.push([
+                    {text: branch_name, style: 'header', colSpan: 10}, "", "", "", "", "", "", "", "", ""
+                ]);
+
+                branches.push([
+                    {text: 'S.No', style: 'header'},
+                    {text: 'UHID', style: 'header'},
+                    {text: 'Patient Name', style: 'header'},
+                    {text: 'Admission Date', style: 'header'},
+                    {text: 'Stay Duration', style: 'header'},
+                    {text: 'Room', style: 'header'},
+                    {text: 'Total Charges', style: 'header'},
+                    {text: 'Consultant', style: 'header'},
+                    {text: 'Advance', style: 'header'},
+                    {text: 'Due', style: 'header'}
+                ]);
+
+                var serial_no = 1;
+                angular.forEach(details, function (row, key) {
+                    var s_no_string = serial_no.toString();
+                    branches.push([
+                        s_no_string,
+                        row.apptPatientData.patient_global_int_code,
+                        row.apptPatientData.fullname,
+                        row.encounter_date,
+                        row.stay_duration.toString(),
+                        row.apptPatientData.current_room,
+                        (row.viewChargeCalculation.total_charge - row.viewChargeCalculation.total_concession).toString(),
+                        row.patient.consultant_name,
+                        (row.viewChargeCalculation.total_paid || '0.00').toString(),
+                        row.viewChargeCalculation.balance.toString(),
+                    ]);
+                    serial_no++;
+                });
+
+                content_info.push({
+                    columns: [
+                        {
+                            text: [
+                                {text: 'Generated On: ', bold: true},
+                                generated_on
+                            ],
+                            margin: [0, 0, 0, 20]
+                        },
+                        {
+                            text: [
+                                {text: ' Generated By: ', bold: true},
+                                generated_by
+                            ],
+                            margin: [0, 0, 0, 20]
+                        }
+                    ]
+                }, {
+                    style: 'demoTable',
+                    table: {
+                        headerRows: 1,
+                        widths: ['*', '*','*', '*','*', '*','*', '*','*', '*'],
+                        body: branches,
+                    },
+                    pageBreak: (index === result_count ? '' : 'after'),
+                });
+                content.push(content_info);
+                index++;
+            });
+            return content;
+        }
+
+        $scope.printReport = function () {
+            var docDefinition = {
+                header: $scope.printHeader(),
+                footer: $scope.printFooter(),
+                styles: $scope.printStyle(),
+                content: $scope.printContent(),
+                pageMargins: ($scope.deviceDetector.browser == 'firefox' ? 75 : 50),
+                pageSize: 'A4',
+                pageOrientation: 'landscape',
+            };
+            pdfMake.createPdf(docDefinition).print();
+        }
     }]);
--- tpl/myworks_report/ipDoctorsPay.js
+++ tpl/myworks_report/ipDoctorsPay.js
@@ -81,10 +81,10 @@ app.controller('ipDoctorsPay', ['$rootScope', '$scope', '$timeout', '$http', '$s
         $scope.parseFloat = function (row) {
             return parseFloat(row);
         }
-
+        
         $scope.printHeader = function () {
             return {
-                text: $scope.app.name + " ( " + $scope.app.org_name + " )",
+                text: "IP Doctors Pay",
                 margin: 5,
                 alignment: 'center'
             };
@@ -119,73 +119,161 @@ app.controller('ipDoctorsPay', ['$rootScope', '$scope', '$timeout', '$http', '$s
 
         $scope.printContent = function () {
             var content = [];
-            var monthly_pay = $filter('groupBy')($scope.records, 'consultant_name');
+            var consultant_wise = $filter('groupBy')($scope.records, 'consultant_name');
 
-            var result_count = Object.keys(monthly_pay).length;
+            var result_count = Object.keys(consultant_wise).length;
             var index = 1;
-            angular.forEach(monthly_pay, function (value, key) {
-                var items = [];
+            angular.forEach(consultant_wise, function (details, doctor_name) {
+                var content_info = [];
+                var date_rage = moment($scope.data.from).format('YYYY-MM-DD') + " - " + moment($scope.data.to).format('YYYY-MM-DD');
+                var generated_on = $scope.generated_on;
+                var generated_by = $scope.app.username;
+                var consultant_wise_total = 0;
 
-                items[0] = [{text: 'S.No', style: 'header'},
-                    {text: 'Patient Name', style: 'header'},
-                    {text: 'UHID', style: 'header'},
-                    {text: 'No.of.Visits', style: 'header'},
-                    {text: 'Amount', style: 'header'}];
+                //Branchwise
+                var branch_wise = $filter('groupBy')(details, 'branch_name');
 
-                var serial_no = 1;
-                var total = 0;
-                angular.forEach(value, function (patient_infos) {
-                    var s_no_string = serial_no.toString();
-                    items[serial_no] = [
-                        s_no_string,
-                        patient_infos.patient_name,
-                        patient_infos.patient_global_int_code,
-                        patient_infos.total_visit,
-                        patient_infos.total_charge_amount
-                    ];
-                    total += parseFloat(patient_infos.total_charge_amount);
-                    serial_no++;
+                var branches = [];
+                branches.push(
+                        [{text: 'Branches', alignment: 'center', style: 'header', colSpan: 2}, ""],
+                        [{text: 'Branch Name', style: 'header'}, {text: 'Amount', style: 'header'}]
+                        );
+
+                angular.forEach(branch_wise, function (branch, branch_name) {
+                    var branch_wise_total = 0;
+
+                    angular.forEach(branch, function (record, key) {
+                        branch_wise_total += parseFloat(record.total_charge_amount);
+                        consultant_wise_total += parseFloat(record.total_charge_amount);
+                    });
+
+                    var branch_total = branch_wise_total.toString();
+
+                    branches.push([
+                        {text: branch_name},
+                        {text: branch_total, alignment: 'right'}
+                    ]);
                 });
-                items[serial_no] = ["", {
-                        text: "Total",
-                        colSpan: 3,
-                        alignment: 'right',
-                        style: 'header'
-                    }, "", "", {
-                        text: total.toString(),
-                        style: 'header'
-                    }];
+                branches.push([
+                    {
+                        text: 'Total',
+                        style: 'header',
+                        alignment: 'right'
+                    },
+                    {
+                        text: consultant_wise_total.toString(),
+                        style: 'header',
+                        alignment: 'right'
+                    }
+                ]);
 
-                content.push({
-                    text: 'To,'
-                },
-                {
-                    text: key,
-                    margin: [20, 10, 0, 10]
-                },
-                {
-                    text: 'Dear Doctor,'
-                },
-                {
-                    text: 'Thank you for your support to us for your patients.  I wish your satisfaction towards the quality of our service',
-                    margin: [0, 10, 0, 10]
-                },
-                {
-                    text: 'I hereby enclose the details and the payment due for you',
-                    margin: [0, 10, 0, 10]
-                },
-                {
+                content_info.push({
+                    columns: [
+                        {
+                            text: [
+                                {text: 'Name of the Doctor: ', bold: true},
+                                doctor_name
+                            ],
+                            margin: [0, 0, 0, 5]
+                        },
+                        {
+                            text: [
+                                {text: 'Generated On: ', bold: true},
+                                generated_on
+                            ],
+                            margin: [0, 0, 0, 5]
+                        }
+                    ]
+                }, {
+                    columns: [
+                        {
+                            text: [
+                                {text: 'Date Range: ', bold: true},
+                                date_rage
+                            ],
+                            margin: [0, 0, 0, 5]
+                        },
+                        {
+                            text: [
+                                {text: ' Generated By: ', bold: true},
+                                generated_by
+                            ],
+                            margin: [0, 0, 0, 5]
+                        }
+                    ]
+                }, {
+                    text: [
+                        {text: 'Total Amount: ', bold: true},
+                        consultant_wise_total.toString()
+                    ],
+                    margin: [0, 0, 0, 5]
+                }, {
                     style: 'demoTable',
                     table: {
                         headerRows: 1,
-                        widths: ['*', '*', '*', '*', '*'],
-                        body: items,
+                        widths: ['*', '*'],
+                        body: branches,
                     },
-                    pageBreak: (index === result_count ? '' : 'after'),
+                    margin: [0, 0, 0, 20]
                 });
+
+                angular.forEach(branch_wise, function (branch, branch_name) {
+                    var items = [];
+
+                    items.push([
+                        {text: branch_name, style: 'header', colSpan: 5}, "", "", "", ""
+                    ]);
+
+                    items.push([
+                        {text: 'S.No', style: 'header'},
+                        {text: 'Patient Name', style: 'header'},
+                        {text: 'UHID', style: 'header'},
+                        {text: 'No.of.Visits', style: 'header'},
+                        {text: 'Amount', style: 'header'}
+                    ]);
+
+                    var items_serial_no = 1;
+                    var total = 0;
+
+                    angular.forEach(branch, function (record, key) {
+                        var s_no_string = items_serial_no.toString();
+                        items.push([
+                            s_no_string,
+                            record.patient_name,
+                            record.patient_global_int_code,
+                            record.total_visit,
+                            record.total_charge_amount
+                        ]);
+                        total += parseFloat(record.total_charge_amount);
+                        items_serial_no++;
+                    });
+
+                    items.push([
+                        {
+                            text: "Total",
+                            colSpan: 4,
+                            alignment: 'right',
+                            style: 'header'
+                        }, "", "", "", {
+                            text: total.toString(),
+                            style: 'header'
+                        }
+                    ]);
+
+                    content_info.push({
+                        style: 'demoTable',
+                        table: {
+                            headerRows: 1,
+                            widths: ['*', '*', '*', '*', '*'],
+                            body: items,
+                        },
+                        margin: [0, 0, 0, 20],
+                        pageBreak: (index === result_count ? '' : 'after'),
+                    });
+                });
+                content.push(content_info);
                 index++;
             });
-
             return content;
         }
 
@@ -200,4 +288,5 @@ app.controller('ipDoctorsPay', ['$rootScope', '$scope', '$timeout', '$http', '$s
             };
             pdfMake.createPdf(docDefinition).print();
         }
+
     }]);
