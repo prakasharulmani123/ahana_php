8eba97c3a0999b6b25548d63da1a2fe42767b1b1
Thangaraj-ark <thangarajp@arkinfotec.com>
2017-05-11 13:57:39 +0530

    In master city table changed to datatable concept.

--- IRISORG/modules/v1/controllers/CityController.php
+++ IRISORG/modules/v1/controllers/CityController.php
@@ -59,4 +59,68 @@ class CityController extends ActiveController {
         }
     }
     
+    public function actionGetcities() {
+        $requestData = $_REQUEST;
+
+        $modelClass = $this->modelClass;
+        $totalData = $modelClass::find()->count();
+        $totalFiltered = $totalData;
+        
+        // Order Records
+        if (isset($requestData['order'])) {
+            if ($requestData['order'][0]['dir'] == 'asc') {
+                $sort_dir = SORT_ASC;
+            } elseif ($requestData['order'][0]['dir'] == 'desc') {
+                $sort_dir = SORT_DESC;
+            }
+            $order_array = [$requestData['columns'][$requestData['order'][0]['column']]['data'] => $sort_dir];
+        }
+
+        if (!empty($requestData['search']['value'])) {
+            $tenant_id = Yii::$app->user->identity->logged_tenant_id;
+            $totalFiltered = $modelClass::find()
+                    ->andFilterWhere([
+                        'OR',
+                            ['like', 'city_name', $requestData['search']['value']],
+                            ])
+                    ->count();
+
+            $cities = $modelClass::find()
+                    ->andFilterWhere([
+                        'OR',
+                            ['like', 'city_name', $requestData['search']['value']],
+                            ])
+                    ->limit($requestData['length'])
+                    ->offset($requestData['start'])
+                    ->orderBy($order_array)
+                    ->all();
+        } else {
+            $cities = $modelClass::find()
+                    ->limit($requestData['length'])
+                    ->offset($requestData['start'])
+                    ->orderBy($order_array)
+                    ->all();
+        }
+
+        $data = array();
+        foreach ($cities as $city) {
+            $nestedData = array();
+            $nestedData['city_id'] = $city->city_id;
+            $nestedData['city_name'] = $city->city_name;
+            $nestedData['tenant_id'] = $city->tenant_id;
+            $nestedData['state_id'] = $city->state_id;
+            $nestedData['status'] = $city->status;
+            $data[] = $nestedData;
+        }
+
+        $json_data = array(
+            "draw" => intval($requestData['draw']),
+            "recordsTotal" => intval($totalData),
+            "recordsFiltered" => intval($totalFiltered),
+            "data" => $data   // total data array
+        );
+
+        echo json_encode($json_data);
+    }
+    
 }
