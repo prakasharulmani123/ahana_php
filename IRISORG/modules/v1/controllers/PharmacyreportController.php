ea22e48860a15da80934a8e71c03f9fce5bff7c7
s-nadesh <nadesh@arkinfotec.com>
2017-06-21 18:43:21 +0530

    Stock report date wise

--- IRISORG/modules/v1/controllers/PharmacyreportController.php
+++ IRISORG/modules/v1/controllers/PharmacyreportController.php
@@ -142,39 +142,49 @@ class PharmacyreportController extends ActiveController {
         $stock_report = $command->queryAll();
         return ['stock_report' => $stock_report];
     }
-
-    //Not using now 
+    
     public function actionStockreport() {
         $post = Yii::$app->getRequest()->post();
-        $tenant_id = Yii::$app->user->identity->logged_tenant_id;
-
-        $stocks = PhaProductBatch::find()
-                ->joinWith('product')
-                ->joinWith('phaProductBatchRate')
-                ->andWhere(['pha_product.tenant_id' => $tenant_id])
-                ->addSelect([
-                    "CONCAT(
-                        IF(pha_product.product_name IS NULL OR pha_product.product_name = '', ' ', pha_product.product_name),
-                        IF(pha_product.product_unit_count IS NULL OR pha_product.product_unit_count = '', ' ', CONCAT(' | ', pha_product.product_unit_count)),
-                        IF(pha_product.product_unit IS NULL OR pha_product.product_unit = '', ' ', CONCAT(' | ', pha_product.product_unit))
-                    ) as product_name",
-                    'SUM(available_qty) as available_qty',
-                    'pha_product.product_code as product_code',
-                    'pha_product_batch_rate.mrp as mrp'])
-                ->groupBy(['pha_product.product_id'])
-                ->all();
-
-        $reports = [];
-
-        foreach ($stocks as $key => $purchase) {
-            $reports[$key]['product_name'] = $purchase['product_name'];
-            $reports[$key]['product_code'] = $purchase['product_code'];
-            $reports[$key]['mrp'] = $purchase['mrp'];
-            $reports[$key]['available_qty'] = $purchase['available_qty'];
-            $reports[$key]['stock_value'] = $purchase['mrp'] * $purchase['available_qty'];
+        if(!empty($post)) {
+            $conncection = Yii::$app->client;
+            $command = $conncection->createCommand("CALL pha_stock_report_by_date({$post['tenant_id']}, '{$post['from']}');");
+            $stock_report = $command->queryAll();
+            return ['stock_report' => $stock_report];
         }
-
-        return ['report' => $reports];
     }
+
+    //Not using now 
+//    public function actionStockreport() {
+//        $post = Yii::$app->getRequest()->post();
+//        $tenant_id = Yii::$app->user->identity->logged_tenant_id;
+//
+//        $stocks = PhaProductBatch::find()
+//                ->joinWith('product')
+//                ->joinWith('phaProductBatchRate')
+//                ->andWhere(['pha_product.tenant_id' => $tenant_id])
+//                ->addSelect([
+//                    "CONCAT(
+//                        IF(pha_product.product_name IS NULL OR pha_product.product_name = '', ' ', pha_product.product_name),
+//                        IF(pha_product.product_unit_count IS NULL OR pha_product.product_unit_count = '', ' ', CONCAT(' | ', pha_product.product_unit_count)),
+//                        IF(pha_product.product_unit IS NULL OR pha_product.product_unit = '', ' ', CONCAT(' | ', pha_product.product_unit))
+//                    ) as product_name",
+//                    'SUM(available_qty) as available_qty',
+//                    'pha_product.product_code as product_code',
+//                    'pha_product_batch_rate.mrp as mrp'])
+//                ->groupBy(['pha_product.product_id'])
+//                ->all();
+//
+//        $reports = [];
+//
+//        foreach ($stocks as $key => $purchase) {
+//            $reports[$key]['product_name'] = $purchase['product_name'];
+//            $reports[$key]['product_code'] = $purchase['product_code'];
+//            $reports[$key]['mrp'] = $purchase['mrp'];
+//            $reports[$key]['available_qty'] = $purchase['available_qty'];
+//            $reports[$key]['stock_value'] = $purchase['mrp'] * $purchase['available_qty'];
+//        }
+//
+//        return ['report' => $reports];
+//    }
 
 }
