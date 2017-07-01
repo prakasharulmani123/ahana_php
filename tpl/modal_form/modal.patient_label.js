8c941f938cfcbb072a45cfdc187895623407e955
Thangaraj-ark <thangarajp@arkinfotec.com>
2017-07-01 15:24:08 +0530

    Label print changes.

--- tpl/modal_form/modal.patient_label.js
+++ tpl/modal_form/modal.patient_label.js
@@ -16,7 +16,8 @@ app.controller('PatientLabelController', ['scope', '$scope', function (scope, $s
         $scope.imgExport = function (imgID) {
             var img = document.getElementById(imgID);
             var canvas = document.createElement("canvas");
-            canvas.width = img.width;
+            //canvas.width = img.width;
+            canvas.width = 210;
             canvas.height = img.height;
 
             // Copy the image contents to the canvas
@@ -30,39 +31,49 @@ app.controller('PatientLabelController', ['scope', '$scope', function (scope, $s
             var dataURL = canvas.toDataURL("image/png");
             return dataURL;
         }
-        $scope.textDetail = function (name,len) {
+        $scope.textDetail = function (name, len) {
             var name_count = name.length;
-            if(name_count >= len) return  name.substring(0, len)+'...';
-            else return name;
+            if (name_count >= len)
+                return  name.substring(0, len) + '...';
+            else
+                return name;
         }
-        
+        $scope.toTitleCase = function (str)
+        {
+            return str.replace(/\w\S*/g, function (txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
+        }
+
         $scope.printContent = function () {
             var encoded_image = $('#patient-barcode img').attr('src');
             var content = [];
             content.push(
                     {
                         image: $scope.imgExport('ahana_image'),
-                        width:70,
-                        height:15,
-                        margin: [20, 0, 0, 0],
+                        width: 70,
+                        height: 15,
+                        //margin: [-35, 0, 0, 0],
+                        alignment: 'right',
                     },
                     {
                         fontSize: 7,
+                        bold: true,
+                        lineHeight: 1.2,
                         text: [
-                            'Name: ' + $scope.textDetail($scope.data.view_data.fullname,13),
-                            '\nUHID: ' + $scope.data.view_data.patient_global_int_code,
-                            '\nGender: ' + $scope.data.app.patientDetail.patientSex,
-                            '\nAge: ' + $scope.data.view_data.patient_age,
-                            '\nDr Name: ' + $scope.textDetail($scope.data.view_data.consultant_name,13) + "\n",
+                            //$scope.textDetail($scope.data.view_data.fullname,13),
+                            $scope.toTitleCase($scope.data.view_data.fullname),
+                            '\n' + $scope.data.app.patientDetail.patientSex + '/' + $scope.data.view_data.patient_dob + ' (' + $scope.data.view_data.patient_age + ')',
+                            '\n' + $scope.data.view_data.consultant_name + '\n',
                         ],
-                        margin: [0, 0, 0, 0],
+                        margin: [0, 3, 0, 10],
 
                     },
                     {
                         image: encoded_image,
-                        width: 95,
-                        height: 15,
-
+                        width: 130,
+                        height: 30,
+                        margin: [-4, -3, 0, 0],
                     }
             );
             return content;
@@ -79,7 +90,7 @@ app.controller('PatientLabelController', ['scope', '$scope', function (scope, $s
 //                    pageSize: {width: 2.95 * 72, height: 1.96 * 72}, //8cm
                     pageSize: {width: 1.25 * 72, height: 1.98 * 72}, //8cm
                     pageOrientation: 'landscape',
-                    pageMargins: [10, 10, 50, 0],
+                    pageMargins: [10, 3, 10, 0],
 
                 };
                 var pdf_document = pdfMake.createPdf(docDefinition);
