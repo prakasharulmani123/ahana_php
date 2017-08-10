5d827b89aa06eaacc425a21e13cd1a53dc703710
Thangaraj-ark <thangarajp@arkinfotec.com>
2017-08-10 18:01:52 +0530

    Added audit log in full site.

---
+++ IRISORG/modules/v1/controllers/AuditlogController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace IRISORG\modules\v1\controllers;
+
+use common\models\CoAuditLog;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\db\BaseActiveRecord;
+use yii\filters\auth\QueryParamAuth;
+use yii\filters\ContentNegotiator;
+use yii\rest\ActiveController;
+use yii\web\Response;
+
+class AuditlogController extends ActiveController {
+
+    public $modelClass = 'common\models\CoAuditLog';
+
+    public function behaviors() {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => QueryParamAuth::className()
+        ];
+        $behaviors['contentNegotiator'] = [
+            'class' => ContentNegotiator::className(),
+            'formats' => [
+                'application/json' => Response::FORMAT_JSON,
+            ],
+        ];
+
+        return $behaviors;
+    }
+
+    public function actions() {
+        $actions = parent::actions();
+        $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+
+        return $actions;
+    }
+
+    public function prepareDataProvider() {
+        /* @var $modelClass BaseActiveRecord */
+        $modelClass = $this->modelClass;
+
+        return new ActiveDataProvider([
+            'query' => $modelClass::find()->orderBy(['created_at' => SORT_DESC]),
+            'pagination' => false,
+        ]);
+    }
+    
+    public function actionGetauditlog() {
+        $offset = abs($_REQUEST['pageIndex'] - 1) * $_REQUEST['pageSize'];
+        $audit = CoAuditLog::find()
+                ->limit($_REQUEST['pageSize'])
+                ->offset($offset)
+                ->orderBy(['created_at' => SORT_DESC])
+                ->all();
+        $totalCount = CoAuditLog::find()->count();
+        return ['audit' => $audit, 'totalCount' => $totalCount];
+    }
+
+}
