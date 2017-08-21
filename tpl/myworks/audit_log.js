45c5734950436cf8d475380f8e2eb388b46e95e3
Thangaraj-ark <thangarajp@arkinfotec.com>
2017-08-21 15:25:14 +0530

    Task - Configuration - Audit Log - Export

--- tpl/myworks/audit_log.js
+++ tpl/myworks/audit_log.js
@@ -1,6 +1,9 @@
 app.controller('AuditController', ['$rootScope', '$scope', '$timeout', '$http', '$state', function ($rootScope, $scope, $timeout, $http, $state) {
 
         $scope.loadAllauditlog = function () {
+            $scope.report_menu =false;
+            $scope.log_status = 'grid';
+            $scope.date = '';
             $scope.isLoading = true;
 
             $scope.maxSize = 10;     // Limit number for pagination display number.  
@@ -8,18 +11,6 @@ app.controller('AuditController', ['$rootScope', '$scope', '$timeout', '$http', 
             $scope.pageIndex = 1;   // Current page number. First page is 1.-->  
             $scope.pageSizeSelected = 10; // Maximum number of items per page.
 
-            // Get data's from service
-//            $http.get($rootScope.IRISOrgServiceUrl + '/auditlog')
-//                    .success(function (log) {
-//                        $scope.isLoading = false;
-//                        $scope.rowCollection = log;
-//
-//                        //Avoid pagination problem, when come from other pages.
-//                        $scope.footable_redraw();
-//                    })
-//                    .error(function () {
-//                        $scope.errorData = "An Error has occured while loading roles!";
-//                    });
             $scope.rowCollection = [];  // base collection
             $scope.getAuditlist();
         };
@@ -42,4 +33,45 @@ app.controller('AuditController', ['$rootScope', '$scope', '$timeout', '$http', 
         $scope.pageChanged = function () {
             $scope.getAuditlist();
         };
+
+        //Audit Log Datepicker
+        $scope.open = function ($event, mode) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            switch (mode) {
+                case 'opened':
+                    $scope.opened = true;
+                    break;
+            }
+        };
+
+        $scope.getReport = function () {
+            $scope.isLoading = true;
+            $scope.loadbar('show');
+            $scope.log_status = 'report';
+            $http.post($rootScope.IRISOrgServiceUrl + '/auditlog/getreport?date=' + moment($scope.date).format('YYYY-MM-DD'))
+                    .success(function (log) {
+                        $scope.isLoading = false;
+                        $scope.loadbar('hide');
+                        $scope.records = log.records;
+                        $scope.generated_on = moment().format('YYYY-MM-DD hh:mm A');
+                    })
+                    .error(function () {
+                        $scope.errorData = "An Error has occured while loading products!";
+                    });
+        };
+
+        $scope.clearReport = function () {
+             $scope.report_menu = false;
+            $scope.log_status = 'grid';
+            $scope.date = '';
+            $scope.getAuditlist();
+        };
+        
+        $scope.$watch('date', function (newValue, oldValue) {
+            if (newValue != '' && typeof newValue != 'undefined') {
+                $scope.report_menu=true;
+            }
+        }, true);
     }]);
