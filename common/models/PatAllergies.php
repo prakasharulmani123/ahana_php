96a60c8650f1a4a4f5bd607c2c2c20b6e8ca7195
Thangaraj-ark <thangarajp@arkinfotec.com>
2017-09-01 17:44:36 +0530

    Added allergies model and controller file.

---
+++ common/models/PatAllergies.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace common\models;
+
+use common\models\query\PatAllergiesQuery;
+use yii\db\ActiveQuery;
+
+/**
+ * This is the model class for table "pat_allergies".
+ *
+ * @property integer $pat_allergies_id
+ * @property integer $tenant_id
+ * @property integer $encounter_id
+ * @property integer $patient_id
+ * @property string $notes
+ * @property string $status
+ * @property integer $created_by
+ * @property string $created_at
+ * @property integer $modified_by
+ * @property string $modified_at
+ * @property string $deleted_at
+ */
+class PatAllergies extends RActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'pat_allergies';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['tenant_id', 'encounter_id', 'patient_id', 'notes'], 'required'],
+            [['tenant_id', 'encounter_id', 'patient_id', 'created_by', 'modified_by'], 'integer'],
+            [['notes', 'status'], 'string'],
+            [['created_at', 'modified_at', 'deleted_at'], 'safe'],
+            [['encounter_id'], 'exist', 'skipOnError' => true, 'targetClass' => PatEncounter::className(), 'targetAttribute' => ['encounter_id' => 'encounter_id']],
+            [['patient_id'], 'exist', 'skipOnError' => true, 'targetClass' => PatPatient::className(), 'targetAttribute' => ['patient_id' => 'patient_id']],
+            [['tenant_id'], 'exist', 'skipOnError' => true, 'targetClass' => CoTenant::className(), 'targetAttribute' => ['tenant_id' => 'tenant_id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'pat_allergies_id' => 'Pat Allergies ID',
+            'tenant_id' => 'Tenant ID',
+            'encounter_id' => 'Encounter ID',
+            'patient_id' => 'Patient ID',
+            'notes' => 'Notes',
+            'status' => 'Status',
+            'created_by' => 'Created By',
+            'created_at' => 'Created At',
+            'modified_by' => 'Modified By',
+            'modified_at' => 'Modified At',
+            'deleted_at' => 'Deleted At',
+        ];
+    }
+    
+    public static function find() {
+        return new PatAllergiesQuery(get_called_class());
+    }
+}
