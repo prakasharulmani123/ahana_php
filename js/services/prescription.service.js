166e1a56f515b78c2eb334f12c171cd9b4c920ac
Thangaraj-ark <thangarajp@arkinfotec.com>
2017-09-11 13:49:54 +0530

    Push the new prescription goto top position.

--- js/services/prescription.service.js
+++ js/services/prescription.service.js
@@ -20,34 +20,29 @@ function PrescriptionService($http, $cookieStore, $rootScope, $window, $localSto
         },
         getPrescriptionItems: function () {
             if (items.length > 0) {
-//                var ordered = $filter('orderBy')(items, "available_quantity");
-//                angular.forEach(ordered, function (value, key) {
-//                    value.item_key = key;
-//                });
-//                return ordered;
-                
                 // convert available_quantity to type Number to make to orderBy to work as it should.
                 angular.forEach(items, function (item) {
                     item.available_quantity = parseFloat(item.available_quantity);
                 });
                 
-                //Separate 0 qty products.
-                var zero_qty_products = [];
-                //Remove 0 qty products in the array
-                for (var i = items.length - 1; i >= 0; i--) {
-                    if (items[i].available_quantity <= 0) {
-                        zero_qty_products.push(items[i]);
-                        items.splice(i, 1);
-                    }
-                }
-                //push 0 qty products at top of the array
-                angular.forEach(zero_qty_products, function (zero_qty_product) {
-                    items.unshift(zero_qty_product);
-                });
+//                //Separate 0 qty products.
+//                var zero_qty_products = [];
+//                //Remove 0 qty products in the array
+//                for (var i = items.length - 1; i >= 0; i--) {
+//                    if (items[i].available_quantity <= 0) {
+//                        zero_qty_products.push(items[i]);
+//                        items.splice(i, 1);
+//                    }
+//                }
+//                //push 0 qty products at top of the array
+//                angular.forEach(zero_qty_products, function (zero_qty_product) {
+//                    items.unshift(zero_qty_product);
+//                });
                 
                 angular.forEach(items, function (value, key) {
                     value.item_key = key;
                 });
+                
                 return items;
             }
             return items;
