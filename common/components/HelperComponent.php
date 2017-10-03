407f1c880d036a6d4e78e7cf2e2151d4873e7967
Thangaraj-ark <thangarajp@arkinfotec.com>
2017-10-03 11:15:16 +0530

    1) Fixed age and year issues in registration and update form 2) Changed dob format in update form

--- common/components/HelperComponent.php
+++ common/components/HelperComponent.php
@@ -373,44 +373,54 @@ class HelperComponent extends Component {
 
     public static function getAgeWithMonth($dob) {
         $dob = date('d-m-Y', strtotime($dob));
-        $localtime = getdate();
-        $today = $localtime['mday'] . "-" . $localtime['mon'] . "-" . $localtime['year'];
-        $dob_a = explode("-", $dob);
-        $today_a = explode("-", $today);
-        $dob_d = $dob_a[0];
-        $dob_m = $dob_a[1];
-        $dob_y = $dob_a[2];
-        $today_d = $today_a[0];
-        $today_m = $today_a[1];
-        $today_y = $today_a[2];
-        $years = $today_y - $dob_y;
-        $months = $today_m - $dob_m;
-        if ($today_m . $today_d < $dob_m . $dob_d) {
-            $years--;
-            $months = 12 + $today_m - $dob_m;
-        }
+        $bday = new DateTime($dob);
+        $today = new DateTime(date('d-m-Y'));
+        $diff = $today->diff($bday);
 
-        if ($today_d < $dob_d) {
-            $months--;
-        }
-
-        $firstMonths = array(1, 3, 5, 7, 8, 10, 12);
-        $secondMonths = array(4, 6, 9, 11);
-        $thirdMonths = array(2);
-
-        if ($today_m - $dob_m == 1) {
-            if (in_array($dob_m, $firstMonths)) {
-                array_push($firstMonths, 0);
-            } elseif (in_array($dob_m, $secondMonths)) {
-                array_push($secondMonths, 0);
-            } elseif (in_array($dob_m, $thirdMonths)) {
-                array_push($thirdMonths, 0);
-            }
-        }
         return [
-            'years' => ($years < 0) ? 0 : $years,
-            'months' => ($years < 0) ? 0 : $months
+            'years' => $diff->y,
+            'months' => $diff->m
         ];
+
+//        $dob = date('d-m-Y', strtotime($dob));
+//        $localtime = getdate();
+//        $today = $localtime['mday'] . "-" . $localtime['mon'] . "-" . $localtime['year'];
+//        $dob_a = explode("-", $dob);
+//        $today_a = explode("-", $today);
+//        $dob_d = $dob_a[0];
+//        $dob_m = $dob_a[1];
+//        $dob_y = $dob_a[2];
+//        $today_d = $today_a[0];
+//        $today_m = $today_a[1];
+//        $today_y = $today_a[2];
+//        $years = $today_y - $dob_y;
+//        $months = $today_m - $dob_m;
+//        if ($today_m . $today_d < $dob_m . $dob_d) {
+//            $years--;
+//            $months = 12 + $today_m - $dob_m;
+//        }
+//
+//        if ($today_d < $dob_d) {
+//            $months--;
+//        }
+//
+//        $firstMonths = array(1, 3, 5, 7, 8, 10, 12);
+//        $secondMonths = array(4, 6, 9, 11);
+//        $thirdMonths = array(2);
+//
+//        if ($today_m - $dob_m == 1) {
+//            if (in_array($dob_m, $firstMonths)) {
+//                array_push($firstMonths, 0);
+//            } elseif (in_array($dob_m, $secondMonths)) {
+//                array_push($secondMonths, 0);
+//            } elseif (in_array($dob_m, $thirdMonths)) {
+//                array_push($thirdMonths, 0);
+//            }
+//        }
+//        return [
+//            'years' => ($years < 0) ? 0 : $years,
+//            'months' => ($years < 0) ? 0 : $months
+//        ];
     }
 
 }
