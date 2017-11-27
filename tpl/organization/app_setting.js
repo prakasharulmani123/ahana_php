98686fefc822e8f455aa9ec38ea82898adacecdd
Thangaraj-ark <thangarajp@arkinfotec.com>
2017-11-27 18:31:49 +0530

    Updated stock report price in kknagar

--- tpl/organization/app_setting.js
+++ tpl/organization/app_setting.js
@@ -561,6 +561,76 @@ app.controller('OrganizationController', ['$rootScope', '$scope', '$timeout', '$
         $scope.showphaMastersUpdateErrorLog = function () {
 
         }
+        
+        //Product Price update
+        $scope.initProductPriceParams = function () {
+            $scope.product_price_import_process_text = '';
+            $scope.progress_product_price_imported_rows = $scope.success_product_price_import_rows = $scope.failed_product_price_import_rows = $scope.total_product_price_import_rows = $scope.import_product_price_percent = 0;
+            $scope.product_price_import_error_log = false;
+        }
+
+        $scope.importProductPrice = function () {
+            $scope.initProductPriceParams();
+            $scope.loadbar('show');
+            $scope.product_price_import_process_text = 'Fetching the Excel Data. Please wait until the importing begins. This might take few mins';
+            $scope.import_log = Date.parse(moment().format());
+           //$scope.import_log = '1510558655000';
+            var currentUser = AuthenticationService.getCurrentUser();
+
+            fileUpload.uploadFileToUrl($scope.productPriceImport, $rootScope.IRISOrgServiceUrl + '/pharmacyproduct/productpriceupdate?tenant_id=' + currentUser.credentials.logged_tenant_id + '&import_log=' + $scope.import_log).success(function (response) {
+                if (response.success) {
+                    $scope.total_product_price_import_rows = response.message.total_rows;
+                    $scope.product_price_import_process_text = 'Importing started';
+                    $scope.productPriceUpdateStart(response.message.id, response.message.max_id);
+                } else {
+                    $scope.loadbar('hide');
+                    $scope.product_price_import_process_text = '';
+                    $scope.errorData = response.message;
+                }
+            }).error(function (data, status) {
+                $scope.loadbar('hide');
+                if (status == 422)
+                    $scope.errorData = $scope.errorSummary(data);
+                else
+                    $scope.errorData = data.message;
+            });
+        }
+
+        $scope.productPriceUpdateStart = function (id, max) {
+            $scope.loadbar('show');
+            $http({
+                method: 'POST',
+                url: $rootScope.IRISOrgServiceUrl + '/pharmacyproduct/productpriceupdatestart',
+                data: {id: id, max_id: max, import_log: $scope.import_log},
+            }).success(
+                    function (response) {
+                        if (response.success) {
+                            $scope.success_product_price_import_rows++;
+                            $scope.progress_product_price_imported_rows++;
+                        } else if (response.continue) {
+                            $scope.failed_product_price_import_rows++;
+                            $scope.progress_product_price_imported_rows++;
+                        }
+
+                        $scope.product_price_import_process_text = 'Import progressing (' + $scope.progress_product_price_imported_rows + '/' + $scope.total_product_price_import_rows + ')';
+                        $scope.import_product_price_percent = ($scope.progress_product_price_imported_rows / $scope.total_product_price_import_rows) * 100;
+
+                        if (response.continue) {
+                            $scope.productPriceUpdateStart(response.continue, max);
+                        } else {
+                            $scope.product_price_import_process_text = 'Import completed (' + $scope.progress_product_price_imported_rows + '/' + $scope.total_product_price_import_rows + ')';
+                            $scope.product_price_import_error_log = true;
+                        }
+                        $scope.loadbar('hide');
+                    }
+            ).error(function (data, status) {
+                $scope.loadbar('hide');
+                if (status == 422)
+                    $scope.errorData = $scope.errorSummary(data);
+                else
+                    $scope.errorData = data.message;
+            });
+        }
     }]);
 
 // I provide a request-transformation method that is used to prepare the outgoing
