4b10f480eac74df62be65d10b034c7dc53680f32
Thangaraj-ark <thangarajp@arkinfotec.com>
2018-01-17 14:03:20 +0530

    Task - Pharmacy - Payment Mode - Make Payment

--- tpl/pharmacy_sale/sale_make_payment.js
+++ tpl/pharmacy_sale/sale_make_payment.js
@@ -2,15 +2,15 @@ app.controller('SaleMakePaymentController', ['scope', '$scope', '$modalInstance'
 
         var sale_id = $modalInstance.data.sale_id;
         var checked_sale_id = $modalInstance.data.checked_sale_id;
-        
+
         sale = $modalInstance.data.sale;
-        
+
         var bill_amount = sale.sum_bill_amount;
         var paid = sale.sum_paid_amount;
         var balance = sale.sum_balance_amount;
         var sale_payment_type = sale.payment_type;
         var encounter_id = sale.encounter_id;
-        
+
         $scope.bill_amount = bill_amount;
         $scope.paid = paid;
         $scope.balance = balance;
@@ -20,59 +20,68 @@ app.controller('SaleMakePaymentController', ['scope', '$scope', '$modalInstance'
         $scope.patient_name = sale.patient_name;
         $scope.sale_payment_type = sale_payment_type;
         $scope.encounter_id = encounter_id;
+        $scope.data.payment_mode = 'CA';
 
         $scope.saleitems = [];
-        angular.forEach(sale.items, function(item){
-            if(item.payment_type != 'CA' && item.payment_status != 'C'){
-                if(checked_sale_id == null || checked_sale_id == item.sale_id){
+        angular.forEach(sale.items, function (item) {
+            if (item.payment_type != 'CA' && item.payment_status != 'C') {
+                if (checked_sale_id == null || checked_sale_id == item.sale_id) {
                     item.checked = true;
-                }else{
+                } else {
                     item.checked = false
                 }
                 $scope.saleitems.push(item);
             }
         });
-        
-        
+
+        $rootScope.commonService.GetPaymentModes(function (response) {
+            $scope.paymentModes = response;
+        });
+
+        $rootScope.commonService.GetCardTypes(function (response) {
+            $scope.cardTypes = response;
+        });
+
+
         $scope.open = function ($event) {
             $event.preventDefault();
             $event.stopPropagation();
             $scope.opened = true;
         };
-        
+
         $scope.maxDate = new Date();
-        
+
         $scope.bills = [];
-        
+
         amt = 0;
-        angular.forEach($scope.saleitems, function(item){
-            if(checked_sale_id == null || checked_sale_id == item.sale_id){
+        angular.forEach($scope.saleitems, function (item) {
+            if (checked_sale_id == null || checked_sale_id == item.sale_id) {
                 $scope.bills.push(item.sale_id);
                 var a = item.billings_total_balance_amount;
-                a = a.replace(/\,/g,''); // 1125, but a string, so convert it to number
-                a = parseInt(a,10);
-                
+                a = a.replace(/\,/g, ''); // 1125, but a string, so convert it to number
+                a = parseInt(a, 10);
+
                 amt = amt + a;
             }
         });
         $scope.paid_amount = amt;
         $scope.data.paid_amount = amt;
         $scope.total_select_bill_amount = amt;
-        
-        $scope.updatePaid = function(saleitem){
+
+        $scope.updatePaid = function (saleitem) {
             $scope.paid_amount = 0;
             $scope.data.paid_amount = 0;
             amt = 0;
-            
+
             $scope.bills = [];
-            $('.chk:checked').each(function(){
+            $('.chk:checked').each(function () {
                 $scope.bills.push($(this).data('bill'));
                 var a = $(this).val();
-                a = a.replace(/\,/g,''); // 1125, but a string, so convert it to number
-                a = parseInt(a,10);
+                a = a.replace(/\,/g, ''); // 1125, but a string, so convert it to number
+                a = parseInt(a, 10);
                 amt = amt + a;
             });
-            
+
             $scope.paid_amount = amt;
             $scope.data.paid_amount = amt;
             $scope.total_select_bill_amount = amt;
@@ -80,6 +89,23 @@ app.controller('SaleMakePaymentController', ['scope', '$scope', '$modalInstance'
 
         $scope.saveForm = function () {
             _that = this;
+            if (_that.data.payment_mode != 'CD') {
+                _that.data.card_type = '';
+                _that.data.card_number = '';
+            }
+
+            if (_that.data.payment_mode != 'CH') {
+                _that.data.cheque_no = '';
+            }
+
+            if (_that.data.payment_mode != 'ON') {
+                _that.data.ref_no = '';
+            }
+
+            if ((_that.data.payment_mode != 'ON') && (_that.data.payment_mode != 'CH')) {
+                _that.data.bank_name = '';
+                _that.data.bank_date = '';
+            }
 
             $scope.errorData = "";
             scope.msg.successMessage = "";
@@ -88,7 +114,7 @@ app.controller('SaleMakePaymentController', ['scope', '$scope', '$modalInstance'
             method = 'POST';
             succ_msg = 'Payment added successfully';
 
-            angular.extend(_that.data, {sale_ids: $scope.bills, encounter_id: $scope.encounter_id, payment_type: $scope.sale_payment_type, total_select_bill_amount:$scope.total_select_bill_amount});
+            angular.extend(_that.data, {sale_ids: $scope.bills, encounter_id: $scope.encounter_id, payment_type: $scope.sale_payment_type, total_select_bill_amount: $scope.total_select_bill_amount});
             scope.loadbar('show');
             $http({
                 method: method,
@@ -110,7 +136,7 @@ app.controller('SaleMakePaymentController', ['scope', '$scope', '$modalInstance'
                                 //scope.updateDisplayCollection($scope.encounter_id, response.sales[0]);
                                 scope.loadSaleItemList('CR');
                                 $modalInstance.dismiss('cancel');
-                                
+
 //                                $state.go('pharmacy.sales');
                             }, 1000)
                         }
