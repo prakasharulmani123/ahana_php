8f24be3ceab380bd7aefd148923f19f4a265815d
s-nadesh <nadesh@arkinfotec.com>
2018-01-22 13:55:26 +0530

    Task: Appointment Change Status

--- IRISORG/modules/v1/controllers/AppointmentController.php
+++ IRISORG/modules/v1/controllers/AppointmentController.php
@@ -65,12 +65,53 @@ class AppointmentController extends ActiveController {
                 $amount_words = Yii::$app->hepler->convert_number_to_words((int) ($amount)) . ' Rupees Only';
 
                 $appt_model->save(false);
+                if(isset($post['future_status_date']) && isset($post['future_status_time']) && isset($post['future_consultant_id']) && $post['future_status_date'] != '' && $post['future_status_time'] != '' && $post['future_consultant_id'] != ''){
+                    $this->bookFutureapt($post);
+                }
+                
                 return ['success' => true, 'amount_in_words' => $amount_words, 'bill_no' => $appt_model->encounter->bill_no];
             } else {
                 return ['success' => false, 'message' => Html::errorSummary($appt_model)];
             }
         } else {
             return ['success' => false, 'message' => 'Please Fill the Form'];
+        }
+    }
+    
+    // Booking from change status form.
+    private function bookFutureapt($post) {
+        if (!empty($post)) {
+            $model = new PatEncounter();
+            $appt_model = new PatAppointment();
+            
+            $encounter_date = $post['future_status_date'].' '.$post['future_status_time'];
+
+            $model_attr = [
+                'patient_id' => (isset($post['patient_id']) ? $post['patient_id'] : ''),
+                'encounter_type' => 'OP',
+                'encounter_date' => $encounter_date,                
+                'consultant_id' => @$post['future_consultant_id']
+            ];
+            $model->attributes = $model_attr;
+            
+            $appt_attr = [
+                'patient_id' => (isset($post['patient_id']) ? $post['patient_id'] : ''),
+                'appt_status' => 'B',
+                'status_date' => $post['future_status_date'],                
+                'status_time' => $post['future_status_time'],                
+                'consultant_id' => @$post['future_consultant_id']
+            ];
+            $appt_model->attributes = $appt_attr;
+
+            $valid = $model->validate();
+            $valid = $appt_model->validate() && $valid;
+            
+            if ($valid) {
+                $model->save(false);
+                $appt_model->encounter_id = $model->encounter_id;
+                $appt_model->save(false);
+                return true;
+            } 
         }
     }
 
