197d17133dec27aabec30e299886268438446caa
Thangaraj-ark <thangarajp@arkinfotec.com>
2018-01-30 15:39:38 +0530

    Inpatient ip billing new concept.

--- tpl/patient_payment/patient_payment.js
+++ tpl/patient_payment/patient_payment.js
@@ -197,12 +197,31 @@ app.controller('BillingPaymentController', ['$rootScope', '$scope', '$timeout', 
                 $scope.spinnerbar('show');
                 $scope.billing_data = {};
                 $scope.data.writeoff_amount = '0';
+
+                //Assign of Procedure ids sections
                 $scope.data.procedure_id = [];
+                $scope.data.procedure_amount = 0;
+                $scope.procedure_selectall = true;
+
+                //Assign professional ids sections
                 $scope.data.professional_id = [];
+                $scope.data.professional_amount = 0;
+                $scope.professional_selectall = true;
+
+                //Assign otherCharges ids sections
                 $scope.data.othercharges_id = [];
+                $scope.data.othercharges_amount = 0;
+                $scope.othercharges_selectall = true;
+
+                //Assign pharmcay ids sections
                 $scope.data.pharmacy_id = [];
+                $scope.data.pharmacy_amount = 0;
+                $scope.pharmacy_selectall = true;
+
                 $scope.data.recurring = false;
+                $scope.data.recurring_amount = 0;
                 $scope.data.paid_amount = 0;
+
                 $http({
                     method: 'GET',
                     url: $rootScope.IRISOrgServiceUrl + '/encounter/getpendingamount?encounter_id=' + $state.params.enc_id,
@@ -234,7 +253,7 @@ app.controller('BillingPaymentController', ['$rootScope', '$scope', '$timeout', 
                             $timeout(function () {
                                 $scope.spinnerbar('hide');
                             }, 2000)
-                            
+
                         }
                 ).error(function (data, status) {
                     $scope.loadbar('hide');
@@ -243,50 +262,6 @@ app.controller('BillingPaymentController', ['$rootScope', '$scope', '$timeout', 
                     else
                         $scope.errorData = data.message;
                 });
-//                $scope.total_select_bill_amount = 0;
-//                $scope.data.billing_paid_amount = 0;
-//                $scope.data.pharmacy_paid_amount = 0;
-
-//                $http({
-//                    method: 'GET',
-//                    url: $rootScope.IRISOrgServiceUrl + '/pharmacysale/getsalebilling?addtfields=patient_report&encounter_id=' + $state.params.enc_id,
-//                }).success(
-//                        function (response) {
-//                            var total = 0.00;
-//                            $scope.data.pharmacy_sale_id = [];
-//                            angular.forEach(response.sale, function (value) {
-//                                if (value.billings_total_balance_amount)
-//                                    total = total + $scope.parseFloatIgnoreCommas(value.billings_total_balance_amount);
-//                                $scope.data.pharmacy_sale_id.push(value.sale_id);
-//                            });
-//                            $scope.data.pharmacy_pending_amount = total;
-//                        }
-//                ).error(function (data, status) {
-//                    $scope.loadbar('hide');
-//                    if (status == 422)
-//                        $scope.errorData = $scope.errorSummary(data);
-//                    else
-//                        $scope.errorData = data.message;
-//                });
-//
-//                $http({
-//                    method: 'GET',
-//                    url: $rootScope.IRISOrgServiceUrl + '/encounter/getpendingamount?encounter_id=' + $state.params.enc_id,
-//                }).success(
-//                        function (response) {
-//                            if (response.balance > 0) {
-//                                $scope.data.bill_pending_amount = response.balance - parseFloat($scope.data.pharmacy_pending_amount);
-//                            } else {
-//                                $scope.data.bill_pending_amount = 0;
-//                            }
-//                        }
-//                ).error(function (data, status) {
-//                    $scope.loadbar('hide');
-//                    if (status == 422)
-//                        $scope.errorData = $scope.errorSummary(data);
-//                    else
-//                        $scope.errorData = data.message;
-//                });
             }
         }
 
@@ -309,7 +284,6 @@ app.controller('BillingPaymentController', ['$rootScope', '$scope', '$timeout', 
                 $scope.totalErrormessage = "Write off amount Must be less than to net amount";
                 return false;
             }
-
             post_url = $rootScope.IRISOrgServiceUrl + '/patientbillingpayment/savesettlementbill';
             method = 'POST';
             succ_msg = 'Billing payment saved successfully';
@@ -339,57 +313,6 @@ app.controller('BillingPaymentController', ['$rootScope', '$scope', '$timeout', 
                 else
                     $scope.errorData = data.message;
             });
-
-//            pharmacy = $filter('filter')($scope.data.bills, 'purchase');
-//            billing = $filter('filter')($scope.data.bills, 'billing');
-//
-//            if (parseFloat(_that.data.paid_amount) != parseFloat(_that.data.billing_paid_amount) + parseFloat(_that.data.pharmacy_paid_amount)) {
-//                $scope.totalErrormessage = "Must be Payable amount is equal to total amount";
-//                return false;
-//            }
-//
-//            if ((billing.length > 0) && (_that.data.bill_pending_amount != 0) && (parseFloat(_that.data.bill_pending_amount) < parseFloat(_that.data.billing_paid_amount))) {
-//                $scope.totalErrormessage = '';
-//                $scope.billErrormessage = "Invalid amount";
-//                return false;
-//            }
-//
-//            if ((pharmacy.length > 0) && (_that.data.pharmacy_pending_amount != 0) && (parseFloat(_that.data.pharmacy_pending_amount) < parseFloat(_that.data.pharmacy_paid_amount))) {
-//                $scope.totalErrormessage = '';
-//                $scope.billErrormessage = '';
-//                $scope.pharmacyErrormessage = "Invalid amount";
-//                return false;
-//            }
-//
-//            post_url = $rootScope.IRISOrgServiceUrl + '/patientbillingpayment/savesettlementbill';
-//            method = 'POST';
-//            succ_msg = 'Billing payment saved successfully';
-//            $scope.loadbar('show');
-//            angular.extend(_that.data, {
-//                patient_id: $scope.patientObj.patient_id,
-//                encounter_id: $scope.encounter.encounter_id
-//            });
-//            $http({
-//                method: method,
-//                url: post_url,
-//                data: _that.data,
-//            }).success(
-//                    function (response) {
-//                        $scope.loadbar('hide');
-//                        $scope.msg.successMessage = succ_msg;
-//                        $scope.data = {};
-//                        $timeout(function () {
-//                            $state.go('patient.billing', {id: $state.params.id});
-//                        }, 1000)
-//
-//                    }
-//            ).error(function (data, status) {
-//                $scope.loadbar('hide');
-//                if (status == 422)
-//                    $scope.errorData = $scope.errorSummary(data);
-//                else
-//                    $scope.errorData = data.message;
-//            });
         }
 
         $scope.getTotalamount = function (total_charge_amount, concession_amount, extra_amount) {
@@ -404,40 +327,55 @@ app.controller('BillingPaymentController', ['$rootScope', '$scope', '$timeout', 
                 var amount = parseFloat(total_amount);
             }
             if ($(row_id).is(':checked')) {
-                $scope.data.writeoff_amount = parseFloat($scope.data.writeoff_amount) + amount; //Add Write Off session amount
                 //Push the primary key and foregin key ids
                 if (mode == 'professional') {
                     $scope.data.professional_id.push(pk_mode_id);
+                    $scope.data.professional_amount += amount;
                 } else if (mode == 'procedure') {
                     $scope.data.procedure_id.push(pk_mode_id);
+                    $scope.data.procedure_amount += amount;
                 } else if (mode == 'othercharges') {
                     $scope.data.othercharges_id.push(pk_mode_id);
+                    $scope.data.othercharges_amount += parseFloat(amount);
                 } else if (mode == 'pharmacy') {
                     $scope.data.pharmacy_id.push(pk_mode_id);
+                    $scope.data.pharmacy_amount += amount;
                 } else {
                     $scope.data.recurring = true;
                     $scope.data.recurring_amount = total_amount;
                 }
             } else {
-                $scope.data.writeoff_amount = parseFloat($scope.data.writeoff_amount) - amount; //Minus the write off session amount
                 //Remove the primary key and foregin key ids
                 if (mode == 'professional') {
+                    $('input[name=professional_selectall]').attr('checked', false);
+                    $scope.professional_selectall = false;
                     var index = $scope.data.professional_id.indexOf(pk_mode_id);
                     $scope.data.professional_id.splice(index, 1);
+                    $scope.data.professional_amount -= amount;
                 } else if (mode == 'procedure') {
+                    $('input[name=procedure_selectall]').attr('checked', false);
+                    $scope.procedure_selectall = false;
                     var index = $scope.data.procedure_id.indexOf(pk_mode_id);
                     $scope.data.procedure_id.splice(index, 1);
+                    $scope.data.procedure_amount -= amount;
                 } else if (mode == 'othercharges') {
+                    $('input[name=othercharges_selectall]').attr('checked', false);
+                    $scope.othercharges_selectall = false;
                     var index = $scope.data.othercharges_id.indexOf(pk_mode_id);
                     $scope.data.othercharges_id.splice(index, 1);
+                    $scope.data.othercharges_amount -= amount;
                 } else if (mode == 'pharmacy') {
+                    $('input[name=pharmacy_selectall]').attr('checked', false);
+                    $scope.pharmacy_selectall = false;
                     var index = $scope.data.pharmacy_id.indexOf(pk_mode_id);
                     $scope.data.pharmacy_id.splice(index, 1);
+                    $scope.data.pharmacy_amount -= amount;
                 } else {
                     $scope.data.recurring = false;
-                    $scope.data.recurring_amount = '';
+                    $scope.data.recurring_amount = 0;
                 }
             }
+            $scope.data.writeoff_amount = parseFloat($scope.data.professional_amount) + parseFloat($scope.data.procedure_amount) + parseFloat($scope.data.othercharges_amount) + parseFloat($scope.data.pharmacy_amount) + parseFloat($scope.data.recurring_amount); //Minus the write off session amount
         }
 
         $scope.calculateNetamount = function () {
@@ -447,4 +385,73 @@ app.controller('BillingPaymentController', ['$rootScope', '$scope', '$timeout', 
                 $scope.data.net_amount = parseFloat($scope.data.paid_amount);
             }
         }
+
+        $scope.updateCheckbox = function (mode) {
+            if (mode == 'professional') {
+                if ($("#professional_selectall").is(':checked')) {
+                    $scope.professional_selectall = true;
+                    $scope.data.professional_id = [];
+                    $scope.data.professional_amount = 0;
+                } else {
+                    $scope.professional_selectall = false;
+                }
+
+                angular.forEach($scope.professional, function (row) {
+                    row.selected = $scope.professional_selectall;
+                });
+                $timeout(function () {
+                    angular.forEach($scope.professional, function (row, ip_key) {
+                        $scope.addChargesAmount(ip_key, 'professional', row.total_charge_amount, row.concession_amount, row.extra_amount, row.consultant_id)
+                    });
+                }, 800);
+            } else if (mode == 'procedure') {
+                if ($("#procedure_selectall").is(':checked')) {
+                    $scope.procedure_selectall = true;
+                    $scope.data.procedure_id = [];
+                    $scope.data.procedure_amount = 0;
+                } else {
+                    $scope.procedure_selectall = false;
+                }
+                angular.forEach($scope.procedure, function (row) {
+                    row.selected = $scope.procedure_selectall;
+                });
+                $timeout(function () {
+                    angular.forEach($scope.procedure, function (row, ip_key) {
+                        $scope.addChargesAmount(ip_key, 'procedure', row.total_charge_amount, row.concession_amount, row.extra_amount, row.charge_subcat_id)
+                    });
+                }, 800);
+            } else if (mode == 'othercharges') {
+                if ($("#othercharges_selectall").is(':checked')) {
+                    $scope.othercharges_selectall = true;
+                    $scope.data.othercharges_id = [];
+                    $scope.data.othercharges_amount = 0;
+                } else {
+                    $scope.othercharges_selectall = false;
+                }
+                angular.forEach($scope.otherCharges, function (row) {
+                    row.selected = $scope.othercharges_selectall;
+                });
+                $timeout(function () {
+                    angular.forEach($scope.otherCharges, function (row, ip_key) {
+                        $scope.addChargesAmount(ip_key, 'othercharges', row.total_charge_amount, row.concession_amount, row.extra_amount, row.charge_subcat_id)
+                    });
+                }, 800);
+            } else {
+                if ($("#pharmacy_selectall").is(':checked')) {
+                    $scope.pharmacy_selectall = true;
+                    $scope.data.pharmacy_id = [];
+                    $scope.data.pharmacy_amount = 0;
+                } else {
+                    $scope.pharmacy_selectall = false;
+                }
+                angular.forEach($scope.pharmacyCharges, function (row) {
+                    row.selected = $scope.pharmacy_selectall;
+                });
+                $timeout(function () {
+                    angular.forEach($scope.pharmacyCharges, function (row, ip_key) {
+                        $scope.addChargesAmount(ip_key, 'pharmacy', row.billings_total_balance_amount, '0', '0', row.sale_id)
+                    });
+                }, 800);
+            }
+        };
     }]);
