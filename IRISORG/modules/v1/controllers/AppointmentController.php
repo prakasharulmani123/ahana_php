b2ee0f18a3aedb90856d3eb81a1bf6142ac18130
Thangaraj-ark <thangarajp@arkinfotec.com>
2018-06-05 16:50:55 +0530

    Task - Bc-T142 OP Procedure

--- IRISORG/modules/v1/controllers/AppointmentController.php
+++ IRISORG/modules/v1/controllers/AppointmentController.php
@@ -4,6 +4,7 @@ namespace IRISORG\modules\v1\controllers;
 
 use common\models\PatAppointment;
 use common\models\PatEncounter;
+use common\models\PatProcedure;
 use Yii;
 use yii\data\ActiveDataProvider;
 use yii\db\BaseActiveRecord;
@@ -65,10 +66,21 @@ class AppointmentController extends ActiveController {
                 $amount_words = Yii::$app->hepler->convert_number_to_words((int) ($amount)) . ' Rupees Only';
 
                 $appt_model->save(false);
-                if(isset($post['future_status_date']) && isset($post['future_status_time']) && isset($post['future_consultant_id']) && $post['future_status_date'] != '' && $post['future_status_time'] != '' && $post['future_consultant_id'] != ''){
+                if (!empty($post['charge_subcat_id'])) {
+                    foreach ($post['charge_subcat_id'] as $key => $value) {
+                        $procedure = new PatProcedure();
+                        $procedure->encounter_id = $post['encounter_id'];
+                        $procedure->patient_id = $post['patient_id'];
+                        $procedure->charge_subcat_id = $value;
+                        $procedure->proc_date = $post['proc_date'];
+                        $procedure->proc_consultant_ids = array($appt_model->consultant_id);
+                        $procedure->save(false);
+                    }
+                }
+                if (isset($post['future_status_date']) && isset($post['future_status_time']) && isset($post['future_consultant_id']) && $post['future_status_date'] != '' && $post['future_status_time'] != '' && $post['future_consultant_id'] != '') {
                     $this->bookFutureapt($post);
                 }
-                
+
                 return ['success' => true, 'amount_in_words' => $amount_words, 'bill_no' => $appt_model->encounter->bill_no];
             } else {
                 return ['success' => false, 'message' => Html::errorSummary($appt_model)];
@@ -77,41 +89,41 @@ class AppointmentController extends ActiveController {
             return ['success' => false, 'message' => 'Please Fill the Form'];
         }
     }
-    
+
     // Booking from change status form.
     private function bookFutureapt($post) {
         if (!empty($post)) {
             $model = new PatEncounter();
             $appt_model = new PatAppointment();
-            
-            $encounter_date = $post['future_status_date'].' '.$post['future_status_time'];
+
+            $encounter_date = $post['future_status_date'] . ' ' . $post['future_status_time'];
 
             $model_attr = [
                 'patient_id' => (isset($post['patient_id']) ? $post['patient_id'] : ''),
                 'encounter_type' => 'OP',
-                'encounter_date' => $encounter_date,                
+                'encounter_date' => $encounter_date,
                 'consultant_id' => @$post['future_consultant_id']
             ];
             $model->attributes = $model_attr;
-            
+
             $appt_attr = [
                 'patient_id' => (isset($post['patient_id']) ? $post['patient_id'] : ''),
                 'appt_status' => 'B',
-                'status_date' => $post['future_status_date'],                
-                'status_time' => $post['future_status_time'],                
+                'status_date' => $post['future_status_date'],
+                'status_time' => $post['future_status_time'],
                 'consultant_id' => @$post['future_consultant_id']
             ];
             $appt_model->attributes = $appt_attr;
 
             $valid = $model->validate();
             $valid = $appt_model->validate() && $valid;
-            
+
             if ($valid) {
                 $model->save(false);
                 $appt_model->encounter_id = $model->encounter_id;
                 $appt_model->save(false);
                 return true;
-            } 
+            }
         }
     }
 
@@ -163,8 +175,8 @@ class AppointmentController extends ActiveController {
                 //Create New Enconter and Book.
                 $en_model = new PatEncounter();
                 $appt_model = new PatAppointment();
-                $encounter_date = $post['data']['status_date'].' '.$value['status_time'];
-                
+                $encounter_date = $post['data']['status_date'] . ' ' . $value['status_time'];
+
                 $model_attr = [
                     'patient_id' => $appointment->patient_id,
                     'encounter_type' => 'OP',
