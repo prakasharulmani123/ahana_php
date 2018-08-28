b8c672c516e55e3b785becd46c547ed8b560628a
Thangaraj-ark <thangarajp@arkinfotec.com>
2018-08-28 15:32:39 +0530

    Added voucher_print.css file

--- tpl/patient_billing/patient_billing.js
+++ tpl/patient_billing/patient_billing.js
@@ -651,7 +651,7 @@ app.controller('BillingController', ['$rootScope', '$scope', '$timeout', '$http'
                 var innerContents = document.getElementById(id).innerHTML;
                 var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=yes,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
                 popupWinindow.document.open();
-                popupWinindow.document.write('<html><head><link href="css/print.css" rel="stylesheet" type="text/css" /></head><body onload="window.print()">' + innerContents + '</html>');
+                popupWinindow.document.write('<html><head><link href="css/voucher_print.css" rel="stylesheet" type="text/css" /></head><body onload="window.print()">' + innerContents + '</html>');
                 popupWinindow.document.close();
             }, 1000)
         }
