aa0ea171fbc8e737b8d1bdf7b815fffbf58b0a45
Thangaraj-ark <thangarajp@arkinfotec.com>
2018-10-25 12:41:02 +0530

    Bc-218 Date Format

--- common/models/PatEncounter.php
+++ common/models/PatEncounter.php
@@ -623,7 +623,7 @@ class PatEncounter extends RActiveRecord {
     }
 
     public function getEncounterFulldetails($model) {
-        return ucwords("{$this->encounter_id} [" . date('d/m/Y', strtotime($this->encounter_date)) . " | {$model->tenant->tenant_name}]");
+        return ucwords("{$this->encounter_id} [" . date('d-m-Y', strtotime($this->encounter_date)) . " | {$model->tenant->tenant_name}]");
     }
 
 //            public function getTotalConcession() {
