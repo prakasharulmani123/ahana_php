fa21851ca3d30854d44259903e003cd673111965
Thangaraj-ark <thangarajp@arkinfotec.com>
2019-02-21 16:42:24 +0530

    Pharmacy purchase calculation changes

--- tpl/pharmacy_purchase/pharmacy_purchase.js
+++ tpl/pharmacy_purchase/pharmacy_purchase.js
@@ -607,17 +607,17 @@ app.controller('PurchaseController', ['$rootScope', '$scope', '$timeout', '$http
             var sgst_amount = (((taxable_value * sgst_perc) / 100)).toFixed(2); // Including vat
             
             var purchase_amount = (parseFloat(taxable_value) + parseFloat(cgst_amount) + parseFloat(sgst_amount)).toFixed(2);
-	    
+	    var disc_amount = disc_perc > 0 ? (taxable_value * (disc_perc / 100)).toFixed(2) : 0;
             //After discount amount changed to taxable, cgst, sgst amount
 	    if(disc_perc > 0) {
 		var new_taxable_value = ((disc_perc / 100) * taxable_value).toFixed(2);
                 var taxable_value = (parseFloat(taxable_value) - parseFloat(new_taxable_value)).toFixed(2);
                 var cgst_amount = (((taxable_value * cgst_perc) / 100)).toFixed(2); // Including vat
                 var sgst_amount = (((taxable_value * sgst_perc) / 100)).toFixed(2); // Including vat
-	    }	
+	    }
             
             var total_amount = (parseFloat(taxable_value) + parseFloat(cgst_amount) + parseFloat(sgst_amount)).toFixed(2);
-            var disc_amount = (purchase_amount - total_amount).toFixed(2);
+            //var disc_amount = (purchase_amount - total_amount).toFixed(2);
             //var purchase_amount = (qty * rate).toFixed(2);
             //var disc_amount = disc_perc > 0 ? (purchase_amount * (disc_perc / 100)).toFixed(2) : 0;
             //var total_amount = (purchase_amount - disc_amount).toFixed(2);
